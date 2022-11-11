87f8e6c026d41ca71335b2a67ccaabdfe6bc7b5e
Henri C. Schmidt <packerfan73@gmail.com>
2022-11-11 15:39:20 -0500

    Implement star homoplasy distance

--- scripts/startle_tp.py
+++ scripts/startle_tp.py
@@ -10,7 +10,7 @@ import subprocess as sp
 import pandas as pd
 import networkx as nx
 
-from collections import defaultdict
+from collections import defaultdict, deque
 from loguru import logger
 
 sys.path.append("src/nni")
@@ -63,7 +63,6 @@ def startle_ilp(character_matrix, mutation_priors):
     proc = sp.Popen(cmd, stderr=sp.PIPE)
     for line in iter(proc.stderr.readline, b""):
         line = line.decode("utf-8")
-        print(line)
         m = re.match(r'Step (\d+) -- number of violations: (\d+)', line)
         if m is not None:
             logger.info(f"ILP Step {m.group(1)} has {m.group(2)} violations.")
@@ -74,6 +73,39 @@ def startle_ilp(character_matrix, mutation_priors):
     inferred_tree = from_newick_get_nx_tree(inferred_tree_file)
     shutil.rmtree(subprocess_dir)
     return inferred_tree
+
+"""
+Cell is a row of the character matrix.
+"""
+def star_homoplasy_distance(tree, labeling, cell):
+    # TODO: use weighted distance instead
+    def distance(node, cell):
+        d, visit_child = 0, True
+        for (character, character_labeling) in labeling.items():
+            node_state = character_labeling[node]
+            cell_state = cell[character]
+
+            if node_state != cell_state and cell_state != '-1':
+                d += 1
+
+            if node_state != '0' and cell_state == '0':
+                visit_child = False
+
+        return d, visit_child
+
+    min_distance = np.inf
+
+    stack = deque(['root'])
+    while stack:
+        current_node = stack.pop()
+        d, visit_child = distance(current_node, cell)
+        if d < min_distance:
+            min_distance = d
+        if not visit_child: continue
+        for child in tree[current_node]:
+            stack.append(child)
+
+    return min_distance
 
 if __name__ == "__main__":
     args = parse_args()
@@ -95,4 +127,11 @@ if __name__ == "__main__":
         lenti_tree = startle_ilp(lenti_cm, input_mutation_priors)
         lenti_cm = lenti_cm.astype(str)
 
+        # labeling is a function from (character, nodes) -> state
         parsimony, parsimony_scores, labeling = small_parsimony(mutation_prior_dict, lenti_tree, lenti_cm)
+        lenti_trees[lenti_group] = (labeling, lenti_tree)
+
+        # test_cell = input_character_matrix[input_character_matrix['c0'] == -1].copy().iloc[0].astype(str)
+        # test_cell = {c: s for c, s in test_cell.items()}
+        # cell_distance = star_homoplasy_distance(lenti_tree, labeling, test_cell)
+        # print(cell_distance)
